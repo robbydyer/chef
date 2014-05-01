0632c09d2c50a61554056e7f0a28a48846b1ef3d
Claire McQuin <claire@getchef.com>
2014-05-01 14:38:07 -0700

    add option to abandon chef run if blocked by another for too long

--- lib/chef/run_lock.rb
+++ lib/chef/run_lock.rb
@@ -20,6 +20,9 @@ require 'fcntl'
 if Chef::Platform.windows?
   require 'chef/win32/mutex'
 end
+require 'chef/config'
+require 'chef/exceptions'
+require 'timeout'
 
 class Chef
 
@@ -46,7 +49,9 @@ class Chef
     end
 
     # Acquire the system-wide lock. Will block indefinitely if another process
-    # already has the lock.
+    # already has the lock and Chef::Config[:what_is_a_good_name_for_this] is
+    # not set. Otherwise will block for Chef::Config[:what_is_a_good_name_for_this]
+    # seconds and exit if the lock is not acquired.
     #
     # Each call to acquire should have a corresponding call to #release.
     #
@@ -55,7 +60,15 @@ class Chef
     # Either acquire() or test() methods should be called in order to
     # get the ownership of run_lock.
     def acquire
-      wait unless test
+      if time_to_wait = run_lock_timeout
+        begin
+          Timeout::timeout(time_to_wait) { (time_to_wait == 0.0 ? exit_from_timeout : wait) unless test }
+        rescue Timeout::Error => e
+          exit_from_timeout
+        end
+      else
+        wait unless test
+      end
     end
 
     #
@@ -92,7 +105,6 @@ class Chef
     # Waits until acquiring the system-wide lock.
     #
     def wait
-      runpid = runlock.read.strip.chomp
       Chef::Log.warn("Chef client #{runpid} is running, will wait for it to finish and then run.")
       if Chef::Platform.windows?
         mutex.wait
@@ -144,6 +156,18 @@ class Chef
       @mutex = Chef::ReservedNames::Win32::Mutex.new("Global\\#{runlock_file.gsub(/[\\]/, "/").downcase}")
       mutex.test
     end
+
+    def runpid
+      runlock.read.strip.chomp
+    end
+
+    def run_lock_timeout
+      Chef::Config[:run_lock_timeout] #@TODO: parameter verification?
+    end
+
+    def exit_from_timeout
+      reset # @TODO: Do we need more/other cleanup?
+      raise Chef::Exceptions::RunLockTimeout.new(run_lock_timeout, runpid)
+    end
   end
 end
-
